5ea3bf9f9f6c020a9488eb2779fb80dbfea1eda0
Adrian Herrera <adrian.herrera02@gmail.com>
2020-12-15 07:55:28 +1100

    inst_compiler: fixes

--- inst_compiler.py
+++ inst_compiler.py
@@ -21,20 +21,21 @@ LIB_DIR = DIR.parent  / 'lib'
 def main():
     """The main function."""
     args = sys.argv
+    env = os.environ.copy()
 
     # Determine compiler
     if sys.argv[0].endswith('++'):
-        cc = os.environ.get('LLVM_CXX')
+        cc = env.get('LLVM_CXX')
         if not cc:
             cc = which('clang++')
     else:
-        cc = os.environ.get('LLVM_CC')
+        cc = env.get('LLVM_CC')
         if not cc:
             cc = which('clang')
     if not cc:
         raise Exception('Unable to find clang (set LLVM_CC env variable)')
 
-    if os.environ.get('LLVM_SPLIT_COMPARES'):
+    if env.get('LLVM_SPLIT_COMPARES'):
         plugins = (LIB_DIR / 'split-compares.so',
                    LIB_DIR / 'split-switches.so',
                    LIB_DIR / 'edge-log.so')
@@ -43,17 +44,20 @@ def main():
 
     plugin_opts = ['-fplugin=%s' % plug.resolve() for plug in plugins]
 
-    # Determine build target
+    # Determine build flags
     bit_mode = 32 if '-m32' in args else 64
+    maybe_linking = True
+    if any(arg in args for arg in ('-v', '-c', '-S', '-E')):
+        maybe_linking = False
 
     # Run the build
-    run_args = [cc, *plugin_opts]
+    run_args = [cc, *plugin_opts, '-Qunused-arguments']
     if len(args) > 1:
         run_args.extend([*args[1:]])
-    run_args.extend(['-lstdc++', '-ldl', '-lz',
-                     '-l:%s' % (LIB_DIR / ('libedge-log-rt-%d.a' % bit_mode)),
-                     '-Qunused-arguments'])
-    proc = run(run_args, check=False)
+    if maybe_linking:
+        run_args.extend(['-lstdc++', '-ldl', '-lz',
+                         '-L%s' % LIB_DIR, '-ledge-log-rt-%d' % bit_mode])
+    proc = run(run_args, env=env, check=False)
 
     return proc.returncode
 
